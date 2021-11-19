ff2911730c2d4f4cd7f7b6d24316c97a4f745ad9
AYGJ <jofre1236@gmail.com>
2021-11-19 00:02:27 -0500

    typed of h1

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* Full page */
 new fullpage('#fullpage', {
     //options here
     autoScrolling: true,
@@ -5,11 +6,30 @@ new fullpage('#fullpage', {
     scrollHorizontally: true,
     navigation: true,
     menu: '#menu',
-    anchors: ['inicio', 'sobre','portfolio','contacto','plus'],
-    navigationTooltips: ['inicio', 'sobre mí','portfolio','contacto','+'],
+    anchors: ['inicio', 'sobre', 'portfolio', 'contacto', 'plus'],
+    navigationTooltips: ['inicio', 'sobre mí', 'portfolio', 'contacto', '+'],
     showActiveTooltip: false,
     easing: 'easeInOutCubic',
 });
 
 //métodos
 fullpage_api.setAllowScrolling(true);
+
+
+/* Typing animation */
+window.onload = function () {
+    console.log("loaded")
+    var typed = new Typed('#typed', {
+        strings: ["-Hola `<i class='far fa-hand-peace'></i>`",
+            "-Soy Aguedo Yanac Gerson","-Puedes decirme Goraycode",
+            `<i class="far fa-heart"></i>         
+            <i class="fas fa-laptop-code"></i>
+            <i class="far fa-heart"></i>    `],
+        backSpeed: 100,
+        smartBackspace: true,
+        backDelay: 1500,
+        startDelay: 100,
+        typeSpeed: 80,
+        loop: true,
+    });
+};
