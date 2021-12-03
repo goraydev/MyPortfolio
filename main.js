7677a6b5bd1ae638b8dc7b91dd8595cc0c366eb2
AYGJ <jofre1236@gmail.com>
2021-12-03 17:12:50 -0500

    Delete file Fullpage

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-/* Full page */
-new fullpage('#fullpage', {
-    //options here
-    autoScrolling: true,
-    scrollHorizontally: true,
-    scrollHorizontally: true,
-    navigation: true,
-    menu: '#menu',
-    anchors: ['inicio', 'sobre', 'portfolio', 'contacto', 'plus'],
-    navigationTooltips: ['inicio', 'sobre mí', 'portfolio', 'contacto', '+'],
-    showActiveTooltip: false,
-    easing: 'easeInOutCubic',
-});
-
-//metods
-fullpage_api.setAllowScrolling(true);
-
-
 /* Typing animation */
 window.onload = function () {
     console.log("loaded")
