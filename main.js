185ad3bf68a8a21c01913bd68683a4e1316c364a
AYGJ <jofre1236@gmail.com>
2021-12-03 19:03:07 -0500

    Phase 1 of responsive design

--- main.js
+++ main.js
@@ -29,15 +29,31 @@ window.addEventListener('load', function () {
         responsive: [
             {
                 // screens greater than >= 775px
-                breakpoint: 450,
+                breakpoint: 250,
                 settings: {
                     // Set to `auto` and provide item width to adjust to viewport
                     slidesToShow: 1,
                     slidesToScroll: 1
                 }
             }, {
+                // screens greater than >= 775px
+                breakpoint: 450,
+                settings: {
+                    // Set to `auto` and provide item width to adjust to viewport
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }, {
                 // screens greater than >= 1024px
                 breakpoint: 800,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                // screens greater than >= 1024px
+                breakpoint: 900,
                 settings: {
                     slidesToShow: 3,
                     slidesToScroll: 3
