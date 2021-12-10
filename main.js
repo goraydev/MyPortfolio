9b702992dbab24774b45a1c48a29652e6eb2aad5
AYGJ <jofre1236@gmail.com>
2021-12-10 00:11:05 -0500

    part 1 of newSlideProjects

--- main.js
+++ main.js
@@ -16,50 +16,87 @@ window.onload = function () {
     });
 };
 
-/* GLider JS */
-window.addEventListener('load', function () {
-    new Glider(document.querySelector('.carousel__lista'), {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        draggable: true,
-        dots: '.carousel__indicadores',
-        arrows: {
-            prev: '.carousel__anterior',
-            next: '.carousel__siguiente'
-        },
-        responsive: [
-            {
-                // screens greater than >= 775px
-                breakpoint: 250,
-                settings: {
-                    // Set to `auto` and provide item width to adjust to viewport
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }, {
-                // screens greater than >= 775px
-                breakpoint: 450,
-                settings: {
-                    // Set to `auto` and provide item width to adjust to viewport
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            }, {
-                // screens greater than >= 1024px
-                breakpoint: 800,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                // screens greater than >= 1024px
-                breakpoint: 900,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3
-                }
+
+(function () {
+    "use strict";
+
+    var carousel = document.getElementsByClassName('carousel')[0],
+        slider = carousel.getElementsByClassName('carousel__slider')[0],
+        items = carousel.getElementsByClassName('carousel__slider__item'),
+        prevBtn = carousel.getElementsByClassName('carousel__prev')[0],
+        nextBtn = carousel.getElementsByClassName('carousel__next')[0];
+
+    var width, height, totalWidth, margin = 20,
+        currIndex = 0,
+        interval, intervalTime = 4000;
+
+    function init() {
+        resize();
+        move(Math.floor(items.length / 2));
+        bindEvents();
+
+        timer();
+    }
+
+    function resize() {
+        width = Math.max(window.innerWidth * .25, 275),
+            height = window.innerHeight * .5,
+            totalWidth = width * items.length;
+
+        slider.style.width = totalWidth + "px";
+
+        for (var i = 0; i < items.length; i++) {
+            let item = items[i];
+            item.style.width = (width - (margin * 2)) + "px";
+            item.style.height = height + "px";
+        }
+    }
+
+    function move(index) {
+
+        if (index < 1) index = items.length;
+        if (index > items.length) index = 1;
+        currIndex = index;
+
+        for (var i = 0; i < items.length; i++) {
+            let item = items[i],
+                box = item.getElementsByClassName('item__3d-frame')[0];
+            if (i == (index - 1)) {
+                item.classList.add('carousel__slider__item--active');
+                box.style.transform = "perspective(1200px)";
+            } else {
+                item.classList.remove('carousel__slider__item--active');
+                box.style.transform = "perspective(1200px) rotateY(" + (i < (index - 1) ? 40 : -40) + "deg)";
             }
-        ]
-    });
-});
+        }
+
+        slider.style.transform = "translate3d(" + ((index * -width) + (width / 2) + window.innerWidth / 2) + "px, 0, 0)";
+    }
+
+    function timer() {
+        clearInterval(interval);
+        interval = setInterval(() => {
+            move(++currIndex);
+        }, intervalTime);
+    }
+
+    function prev() {
+        move(--currIndex);
+        timer();
+    }
+
+    function next() {
+        move(++currIndex);
+        timer();
+    }
+
+
+    function bindEvents() {
+        window.onresize = resize;
+        prevBtn.addEventListener('click', () => { prev(); });
+        nextBtn.addEventListener('click', () => { next(); });
+    }
+
+    init();
+
+})();
